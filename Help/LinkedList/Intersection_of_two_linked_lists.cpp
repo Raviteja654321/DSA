f0f4ddeb39756e7c03b4d97173ef7bc5a829bec8
Raviteja654321 <94986564+Raviteja654321@users.noreply.github.com>
2023-02-05 21:44:03 +0530

    Update Intersection_of_two_linked_lists.cpp

--- Help/LinkedList/Intersection_of_two_linked_lists.cpp
+++ Help/LinkedList/Intersection_of_two_linked_lists.cpp
@@ -6,6 +6,57 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+"Method:1"
+    class Solution
+{
+public:
+    int intersectPoint(Node* head1, Node* head2)
+    {
+        int n=0,m=0;
+        Node* temp=head1;
+        while(temp!=NULL)
+        {
+            n++;
+            temp=temp->next;
+        }
+        temp=head2;
+        while(temp!=NULL)
+        {
+            m++;
+            temp=temp->next;
+        }
+        int mini=min(n,m);
+        int maxi=max(n,m);
+        int diff=maxi-mini;
+        if(n>m)
+        {
+            while(diff--)
+            {
+                head1=head1->next;
+            }
+        }
+        else
+        {
+            while(diff--)
+            {
+                head2=head2->next;
+            }
+        }
+        while(head1!=NULL && head2!=NULL)
+        {
+            if(head1==head2)
+            {
+                return head1->data;
+            }
+            head1=head1->next;
+            head2=head2->next;
+        }
+        return -1;
+    }
+};
+
+"Method:2" :
+
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *a, ListNode *b) {
