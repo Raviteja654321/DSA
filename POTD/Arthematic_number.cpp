57a72f6a91d5a0d70724cbf05a7b370515f008c1
Raviteja654321 <94986564+Raviteja654321@users.noreply.github.com>
2023-02-14 13:01:52 +0530

    Create Arthematic_number.cpp

---
+++ POTD/Arthematic_number.cpp
@@ -0,0 +1,17 @@
+// User function Template for C++
+
+class Solution{
+public:
+    int inSequence(int a, int b, int c){
+        if(c==0)
+
+{
+    return a==b;}double n=1+double(b-a)/(double)c;
+        
+        if(ceil(n)==n && n>0)
+        {
+            return true;
+        }
+        return false;
+    }
+};
