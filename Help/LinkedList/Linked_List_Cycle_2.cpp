d267fb4df8bfcb4a6af0e591d8322981983b29b7
Raviteja654321 <94986564+Raviteja654321@users.noreply.github.com>
2023-03-13 00:59:39 +0530

    Update Linked_List_Cycle_2.cpp

--- Help/LinkedList/Linked_List_Cycle_2.cpp
+++ Help/LinkedList/Linked_List_Cycle_2.cpp
@@ -23,3 +23,39 @@ public:
         return NULL;
     }
 };
+
+
+//Your approach of using a hash map to detect the cycle in the linked list is correct.
+//However, to find the node where the cycle begins, you can use Floyd's cycle-finding algorithm
+//(also known as tortoise and hare algorithm) which uses two pointers - a slow pointer and a fast pointer.
+
+//Initially, both pointers start at the head of the linked list. The slow pointer moves one step at a time while the
+//fast pointer moves two steps at a time. If there is a cycle in the linked list, the fast pointer will eventually catch up
+//to the slow pointer. Once they meet, we move the slow pointer to the head of the linked list and keep the fast pointer where it is.
+//Then, we move both pointers one step at a time until they meet again. The node where they meet is the starting node of the cycle.
+
+
+ListNode *detectCycle(ListNode *head) {
+    if (!head || !head->next) {
+        return nullptr; // No cycle if the list is empty or has only one node.
+    }
+    ListNode *slow = head, *fast = head;
+    bool cycleExists = false;
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast) { // Cycle exists.
+            cycleExists = true;
+            break;
+        }
+    }
+    if (!cycleExists) {
+        return nullptr; // No cycle.
+    }
+    slow = head;
+    while (slow != fast) {
+        slow = slow->next;
+        fast = fast->next;
+    }
+    return slow; // Starting node of the cycle.
+}
